a9a873a170e60e425e4924fc9da436b3687d7376
Maggie Lee <maggie.a.lee@gmail.com>
2026-04-13 16:25:41 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,1 +1,8 @@
+import requests
+import csv
 
+URL = "https://results.sos.ga.gov/cdn/results/Georgia/export-40726SpecialElection.json"
+
+response = requests.get(URL)
+data = response.json()
+print(response.status_code)
