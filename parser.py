f161478722d2c94029575cd2033f8ab4f515cfb1
Maggie Lee <maggie.a.lee@gmail.com>
2026-04-16 13:20:05 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import requests
 import csv
 
 URL = "https://results.sos.ga.gov/cdn/results/Georgia/export-40726SpecialElection.json"
+URL = 'https://results.sos.ga.gov/results/public/Georgia/elections/2024NovGen.json'
 
 response = requests.get(URL)
 data = response.json()
