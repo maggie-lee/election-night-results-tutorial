8fb901c1c99a638c7907a8e98b47bcf2bd3ede24
Maggie Lee <maggie.a.lee@gmail.com>
2026-05-04 14:36:22 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,15 +1,66 @@
 import pandas as pd
 import requests
 import json
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
+###### CHANGE THESE FIVE LINES 
 
-# Open the URL and load the .json file as a Python dict
-# Use this sample url or replace it with your own
+my_json_url = 'https://results.sos.ga.gov/cdn/results/Georgia/export-2024NovGen.json'
+my_url = 'https://results.sos.ga.gov/results/public/Georgia/elections/2024NovGen'
+my_source = 'Georgia Secretary of State'
+my_title = 'Georgia Election Night Reporting'
+my_timezone = 'America/New_York'  # Timezone choices: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 
-url = 'https://results.sos.ga.gov/cdn/results/Georgia/export-2024NovGen.json'
-response = requests.get(url)
+######
+
+response = requests.get(my_json_url)
 response_obj = json.loads(response.content)
 print(response_obj.keys())
+
+# First, get high-level summary data for this election.
+# You will use these in Datawrapper to set the visualization properties. https://developer.datawrapper.de/docs/chart-properties
+
+# Get and prettify the election's date. 
+election_date_str = (response_obj['electionDate'])
+election_date_obj = datetime.strptime(election_date_str, '%Y-%m-%d')
+election_date_prettfied = datetime.strftime(election_date_obj, '%B %-d %Y')
+
+# Get the election's name too
+election_name = (response_obj['electionName'])
+
+# Get the timestamp of the last update to the data.
+update_string = response_obj['createdAt']
+
+# it's in this format: 2025-01-30T13:59:59.5881725Z
+# The Z at the end indicates that this is in UTC, not local time.
+# parse it into a datetime object with the correct timezone
+update_object= (datetime.fromisoformat(update_string.replace('Z', '+00:00'))
+				.astimezone(ZoneInfo(my_timezone)))
+
+# Then parse the datetime object into an easily read date: Nov. 21, 2025 08:59 AM
+update_prettified = datetime.strftime(update_object, '%B %d %Y %I:%M %p')
+
+# make a properties dict according to Datawrapper's specs:
+ # https://developer.datawrapper.de/docs/chart-properties
+
+properties_object = {
+	'title' : my_title,
+	'describe' : {
+		'intro' : 'Last Updated ' + update_prettified,
+		'source_name' : my_source,
+		'source_url' : my_url,
+		'description' : election_name + ' ' + election_date_prettfied,
+		'aria-description' : 'Bar graph of ' + my_title + ', last updated at ' + update_prettified + '.  Original data at ' + my_url
+	}
+}
+
+# Write that dict as a json file in github
+# Let's give the file a good descriptive name & date
+
+outfile_name = election_date_str + ' ' + election_name + '.json'
+with open(outfile_name, 'w') as f:
+	json.dump(properties_object, f, indent=4)
 
 
 # Use Pandas to transform the list of ballot options in the nested object into a two-dimensional table 
@@ -88,16 +139,15 @@ reporting_df = counties_df.groupby('name')[['precinctsParticipating', 'precincts
 
 joined = df.merge(reporting_df, left_on='results.ballotItems.name', right_on='name', how='left')
 
-joined.to_csv('joined.csv')
+# joined.to_csv('joined.csv')
 
 
 
 #Then drop the columns Datawrapper doesn't need
-df = df.drop(columns = ['groupResults', 'precinctResults', 'ballotOrder', 'id', 'sum_of_contest', 'name', 'voteCount', 'results.ballotItems.id'])
+joined = joined.drop(columns = ['groupResults', 'precinctResults', 'ballotOrder', 'id', 'sum_of_contest', 'name', 'voteCount', 'results.ballotItems.id'])
 
 # # publish the output to your Github repo as a .csv.
-df.to_csv('state_and_federal_output.csv')
-
-
-
+#  let's give it a good descriptive name
+state_and_federal_outfile_name = election_date_str + ' ' + election_name + '-state-federal' + '.csv'
+joined.to_csv('state_and_federal_outfile_name')
 
